e465cd1aa1d33c0336490b9156bd22179ec3c763
portsungbin <sd61224@gmail.com>
2025-01-24 18:03:17 +0900

    commit

--- src/day13/homework/level01/Prob1.java
+++ src/day13/homework/level01/Prob1.java
@@ -1,28 +1,91 @@
-//package day13.homework.inheritance실습.level01;
-//
-//public class Prob1 {
-//
-//	public static void main(String[] args){
-//
-//		Drink coffee = new Drink("커피",1500,4);
-//		Drink tea = new Drink("녹차",1100,7);
-//		Alcohol wine = new Alcohol("와인", 5000, 3, 15.1);
-//
-//		System.out.println("***** 매 출 전 표 ***** ");
-//		Drink.printTitle();
-//		coffee.printData();
-//		tea.printData();
-//		System.out.println();
-//
-//		Alcohol.printTitle();
-//		wine.printData();
-//
-//		int sum = coffee.getTotalPrice()
-//				  + tea.getTotalPrice()
-//				  + wine.getTotalPrice();
-//
-//		System.out.println();
-//		System.out.println("*** 총금액 " + sum + "원 ***");
-//
-//	}
-//}
+package day13.homework.level01;
+
+class Drink{
+    private String name;
+    private int price;
+    private int count;
+
+    public Drink() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Drink(String name, int price, int count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    public int getTotalPrice() {
+        return price * count;
+    }
+
+    public static void printTitle() {
+        System.out.println("상품명\t단가\t 수량\t 금액");
+    }
+
+    public void printData() {
+        System.out.printf("%s\t%7d\t%3d\t%9d%n",name,price,count,getTotalPrice());
+
+    }
+}
+
+
+class Alcohol extends Drink {
+    private double alcper;
+
+    public Alcohol(){
+    }
+
+    public Alcohol(String name, int price, int count, double alcper) {
+        super(name, price, count);
+        this.alcper = alcper;
+    }
+
+    public static void printTitle() {
+        System.out.println("상품명(도수[%])\t단가 \t수량 \t금액");
+    }
+
+    @Override
+    public void printData() {
+        System.out.printf("%s\t%.1f\t%8d\t%d\t%9d%n",getName(),alcper,getPrice(),getCount(),getTotalPrice());
+    }
+
+}
+
+public class Prob1 {
+
+	public static void main(String[] args){
+
+		Drink coffee = new Drink("커피",1500,4);
+		Drink tea = new Drink("녹차",1100,7);
+		Alcohol wine = new Alcohol("와인", 5000, 3, 15.1);
+
+		System.out.println("***** 매 출 전 표 ***** ");
+		Drink.printTitle();
+		coffee.printData();
+		tea.printData();
+		System.out.println();
+
+		Alcohol.printTitle();
+		wine.printData();
+
+		int sum = coffee.getTotalPrice()
+				  + tea.getTotalPrice()
+				  + wine.getTotalPrice();
+
+		System.out.println();
+		System.out.println("*** 총금액 " + sum + "원 ***");
+
+	}
+}
--- src/day13/homework/level01/Student.java
+++ src/day13/homework/level01/Student.java
@@ -1,12 +1,41 @@
-//package day13.homework.inheritance실습.level01;
-//
-//public class Student {
-//
-//
-//public static void main(String args[]){
-//	Student stu = new Student("장동건", "jspprogram",
-//			500000);
-//	 stu.calcReturnFee();
-//	stu.print();
-//}
-//}
+package day13.homework.level01;
+
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private String subject;
+    private int fee;
+    private double returnFee;
+
+    public Student(){}
+
+    public Student(String name, String subject, int fee) {
+        this.name = name;
+        this.subject = subject;
+        this.fee = fee;
+    }
+
+    void calcReturnFee() {
+        if (subject.equals("javaprogram")) {
+            returnFee = (fee * 25.0) / 100;
+        } else if (subject.equals("jspprogram")) {
+            returnFee = (fee * 20.0) / 100;
+        } else {
+            System.out.println("그런 과정명은 없습니다.");
+        }
+    }
+
+    void print() {
+        System.out.printf("%s 씨의 과정명은 %s이고 교육비는 %d 이며 환급금은 %.1f 입니다.",name,subject,fee,returnFee);
+    }
+
+
+    public static void main(String args[]) {
+        Student stu = new Student("장동건", "jspprogram",
+                500000);
+        stu.calcReturnFee();
+        stu.print();
+    }
+}
